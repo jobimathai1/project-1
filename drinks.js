fe90d6f2f8face73cf47334c9fa1b4b5d9219771
DangerGreen <r.malikgreen@gmail.com>
2018-09-21 15:18:04 -0700

    rough draft of drinks

---
+++ drinks.js
@@ -0,0 +1,123 @@
+//Sweetners, essential to most cocktails these can be switched out to alter the flavor profile of any cocktail
+simpleSyrup = ['sugar', 'water'];
+habeneroSimple = ['sugar', 'water', 'habenero'];
+honeySyrup = ['honey', 'water', 'clove'];
+hibiscusSimple = ['hibiscusFlower', 'sugar', 'water'];
+rosemarySimple = ['rosemary', 'sugar', 'water']
+orgeat;
+
+
+//Spirits the base of the cocktail
+var spirit;
+var vodka;
+var gin;
+var mezcal = ['blanco', 'reposado', 'anejo', 'extra anejo', tequila['blanco', 'reposado', 'anejo', 'extra anejo']];
+var rum = ['agricole', 'bootstrap', 'spiced rum', 'navy strength',];
+var whiskey = ['bourbon', 'canadian whiskey', 'sour mash whiskey', 'scotch', 'irish whiskey', 'japanese whiskey'];
+
+//liqueurs
+var tripleSec
+var stGermaine
+var campari
+var aperol
+var maraschinoCherry
+var sweetVermouth
+
+//Mixers 
+var coconutJuice
+var orangeJuice
+var lemonJuice
+var limeJuice
+var grapefruitJuice
+var pineappleJuice
+var sourMix = ( limeJuice + lemonJuice + orangeJuice + simpleSyrup )
+var sodaWater
+var cola 
+var lemonLimeSoda
+var grapefruitSoda
+var tonicWater
+var gingerAle
+var gingerBeer
+var cranberryJuice
+var oliveBrine
+var onionBrine
+
+//bitters
+var angosturaBitters
+var orangeBitters
+var cherryBitters
+var rhubarbBitters
+var lavenderBitters
+
+//produce
+var blackberries
+var blueberries
+var cocktailOnions
+var olives
+var orangeSlice
+var orangePeel
+var pineappleWedges
+var strawberries
+var luxardoCherries
+var cucumbers
+var arugula
+var basilLeaves
+var cilantro
+var mintLeaves
+var jalapeno
+var pasillaPepper
+var eggWhite
+
+//ice shape, a lot of people dont realize that the shape of ice has a huge impact upon how quickly a cocktail waters down, the smaller the cube the faster the cocktail waters down, with a small exception...
+var crushedIce
+var smallCubes
+var whiskeyCubes
+
+//mixing type
+var shake
+var stir 
+var pourOver
+
+//glass type
+var rocksGlass
+var martiniGlass
+var collinsGlass
+var 
+
+//basic cocktails
+//the most basic cocktail is a martini
+function martini( gin , smallCubes , oliveBine ); {
+    
+}
+
+//next up would be the daquari, most folks think that they are gross frozen drinks but they are delightfully balanced cocktails when made properly.
+function daquari( spirit , limeJuice , simpleSyrup , smallCubes);{
+    
+}
+
+//
+function margarita( tequila , sourMix , tripleSec);{
+
+}
+
+function buck( spirit, limeJuice, simpleSyrup, gingerBeer );{
+
+}
+function sours( spirit, lemonJuice, simpleSyrup, eggWhite, angosturaBitters);{
+
+}
+function negroni( spirit, sweetVermouth, campari, maraschinoCherry, orangePeel);{
+
+}
+function mojito( spirit, limeJuice, simpleSyrup, mintLeaves);{
+
+}
+function collins( spirit, lemonJuice, simpleSyrup, sodaWater);{
+
+}
+function oldFashioned( bourbon, angosturaBitters, orangeBitters, simpleSyrup, whiskeyCubes );{
+
+}
+function maiTai( rum, pineappleJuice, coconutJuice, sourMix, bootstrap );{
+
+}
