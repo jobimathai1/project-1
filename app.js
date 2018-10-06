7d37020e39810b3f62663c938f8866ee2253152f
Jobi Mathai <jobi.mathai@gmail.com>
2018-10-06 10:50:40 -0700

    cleaned up repo

--- app.js
+++
@@ -1,9 +0,0 @@
-
-//use Jquery
-function openNav() {
-    document.getElementById("theSideNav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("theSideNav").style.width = "0";
-}
