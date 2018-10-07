d8f30580e19b2841683f18f2a7d0504f466850aa
Jobi Mathai <jobi.mathai@gmail.com>
2018-10-07 13:58:35 -0700

    created firebase db & added config info js file

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+
+var config = {
+    apiKey: config.FB_KEY,
+    authDomain: "pet-pride.firebaseapp.com",
+    databaseURL: "https://pet-pride.firebaseio.com",
+    projectId: "pet-pride",
+    storageBucket: "",
+    messagingSenderId: "1083593070841"
+  };
+  firebase.initializeApp(config);
