1a8475dcb7543779b6d0bc2bbdcd3ea3196e0961
Jobi Mathai <jobi.mathai@gmail.com>
2018-10-14 14:29:13 -0700

    cleanup

--- javascript/app.js
+++ javascript/app.js
@@ -20,100 +20,26 @@ var email_address = "";
 $("#btn-add").on("click", function(event){
   event.preventDefault();
 
-<<<<<<< HEAD
   first_name = $("#firstName").val();
   last_name = $("#lastName").val();
   mail_address = $("#mailingAddress").val();
   email_address = $("#emailAddress").val();
 
-  dataRef.ref().push({
-=======
-  // Add the values from form fields into the variable
-  first_name = $("#firstName").val().trim();
-  last_name = $("#lastName").val().trim();
-  mail_address = $("#mailingAddress").val().trim();
-  email_address = $("#emailAddress").val().trim();
-
-  // Push the data from the variables into the firebase db
   dataRef.ref("subscribers").push({
->>>>>>> master
     first_name: first_name,
     last_name: last_name,
     mail_address: mail_address,
     email_address: email_address,
-<<<<<<< HEAD
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
-  });
-});
-
-=======
     subscriber_status: "1",
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
+
   // Clear the form fields on submit
   $("#firstName").val("");
   $("#lastName").val("");
   $("#mailingAddress").val("");
   $("#emailAddress").val("");
 });
->>>>>>> master
-// dataRef.ref().on("child_added", function(childSnapshot){
-//   console.log()
-// })
-
-// var queryURL = "https://us19.api.mailchimp.com/3.0/lists/ba051ccc3c/members/"
-
-
-// //Create variables to store what the mailing list form is sending
-// var fName = $("#firstName").val().trim();
-// var lName = $("#lastName").val().trim();
-// var mailAddress = $("#mailingaddress").val().trim();
-// var emailAddress = $("#emailAddress").val().trim();
-
-
-// // xhr = new XMLHttpRequest();
-// // xhr.open('POST', queryURL, true);
-// // xhr.setRequestHeader("Content-Type", "application/json");
-// // xhr.setRequestHeader("Authorization", "Basic YW55dXNlcjpmMWVjOWFhNzU2ZTFhYzMzODA1Y2U4NjIyNDRkNWFjYi11czE5");
-// // xhr.send({
-// //   "email_address":emailAddress,
-// //   "status": "subscribed",
-// //   "merge_fields": {
-// //     "FNAME": fName,
-// //     "LNAME": lName,
-// //     "ADDRESS": mailAddress
-// //   }
-// // })
-
-// // On Mailing List submit do the following:
-
-// var data = JSON.stringify({
-//   // Just testing the API for now. Still need to figure out how to send the data to mailchimp
-//   "email_address": "jobi@jobi.com",
-//   "status": "subscribed",
-//   "merge_fields": {
-//     "FNAME": "jobi",
-//     "LNAME": "mathai",
-//     "ADDRESS": "123 main st",
-//     "PHONE": "3102229999",
-//   }
-// });
-
-// var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === 4) {
-//     console.log(this.responseText);
-//   }
-// });
-
-// xhr.open("POST", "https://us19.api.mailchimp.com/3.0/lists/ba051ccc3c/members/");
-// xhr.setRequestHeader("Content-Type", "application/json");
-// xhr.setRequestHeader("cache-control", "no-cache");
-// xhr.setRequestHeader("username", "anyuser");
-// xhr.setRequestHeader("Postman-Token", "5bfd3b36-9cc7-4a74-ad6e-49e095d2a7b4");
-// xhr.send(data);
 
 
 
