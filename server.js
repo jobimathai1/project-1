8070a6e3b51e8630727c916e4c142da1624a0778
Jobi Mathai <jobi.mathai@gmail.com>
2018-10-19 00:14:43 -0700

    got mailchimp accepting data from mailingList form, thanks to Mike Y

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 var Mailchimp = require('mailchimp-api-v3')
 var jsonParser = bodyParser.json()
 // var apiKeys = require('/config/keys/keys');
-// var mailchimp = new Mailchimp(apiKeys.mailchimpKey)
+var mailchimp = new Mailchimp('f1ec9aa756e1ac33805ce862244d5acb-us19')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 //view engine
@@ -20,47 +20,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Setting up routes for the pages
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/donations', function(req, res){
-    res.sendFile("public/donations.html", {root: __dirname});
+app.get('/donations', function (req, res) {
+    res.sendFile("public/donations.html", { root: __dirname });
 });
 
-app.get('/mailingList', function(req, res){
-    res.sendFile("public/mailingList.html",{root: __dirname});
+app.get('/mailingList', function (req, res) {
+    res.sendFile("public/mailingList.html", { root: __dirname });
 });
 
-app.get('/programs', function(req, res){
-    res.sendFile("public/programs.html",{root: __dirname});
+app.get('/programs', function (req, res) {
+    res.sendFile("public/programs.html", { root: __dirname });
 });
 
-app.get('/aboutus', function(req, res){
-    res.sendFile("public/about.html",{root: __dirname});
+app.get('/aboutus', function (req, res) {
+    res.sendFile("public/about.html", { root: __dirname });
 });
 //end routes setup
 
-// app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/mailchimp', function(req, res){
+//Route to send data to MailChimp & then return user to homepage after submitting form.
+
+app.post('/mailchimp', function (req, res) {
     console.log("this is a test");
-    res.sendFile("public/index.html",{root: __dirname});
+    mailchimp.post('lists/ba051ccc3c/members', {
+        email_address: req.body.eAddress,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: req.body.fName,
+            LNAME: req.body.lName,
+            ADDRESS: req.body.mAddress
+        }
+    })
+    res.sendFile("public/index.html", { root: __dirname });
 });
 
 
 //for Heroku
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`Our app is running on port ${PORT}`);
 });
-
-// //Mailchimp
-// mailchimp.request({
-//     method:'post',
-//     path: '/lists/ba051ccc3c/members/',
-//     body: {
-//         email_address: 'testaccount-jobi@yahoo.com',
-//         status: 'subscribed',
-//     }
-// })
